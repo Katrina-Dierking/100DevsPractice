941137b999dd303084eeac179c04d8748fe0231e
Katrina <katrina@hertimetocode.com>
2023-03-16 10:29:18 -0400

    chapter 12 hw practice

--- src/class12-materials/simple-calculator/js/main.js
+++ src/class12-materials/simple-calculator/js/main.js
@@ -1,9 +1,12 @@
 let total = 0
 
-document.querySelector('#pumpkin').addEventListener('mouseenter', makeZero)
+// document.querySelector('#pumpkin').addEventListener('mouseenter', makeZero)
+document.getElementById('pumpkin').addEventListener('mouseenter', makeZero)
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector('#doubleTheFun').addEventListener('click', dub2)
+document.querySelector('#divideMe').addEventListener('click', div2)
 
 function makeZero() {
   total = 0
@@ -24,3 +27,14 @@ function sub2() {
   total = total - 2
   document.querySelector('#placeToPutResult').innerHTML = total
 }
+
+function dub2() {
+  total *=2
+  document.querySelector('#placeToPutResult').innerHTML = total
+}
+
+function div2() {
+  total /=2
+  document.querySelector('#placeToPutResult').innerHTML = total
+}
+
