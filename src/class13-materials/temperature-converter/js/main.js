0f3b8e3b1dc69ceb583f6079b7317d59c43534a4
Katrina <katrina@hertimetocode.com>
2023-03-16 19:57:03 -0400

    class 13 practice

--- src/class13-materials/temperature-converter/js/main.js
+++ src/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,23 @@
 //Write your pseduo code first! 
+
+
+
+
+//1. What is the temperature in C? 
+
+// 2. Use the formula to plug in the C and find F
+
+// Formula = F = 9/5 C + 32
+
+
+
+document.querySelector("#convert").addEventListener("click", convert);
+
+function convert() {
+
+        let temp = document.querySelector("#sel").value;
+
+        temp *= 9/5 + 32
+
+        document.querySelector('#placeToConvert').innerText = temp
+}
