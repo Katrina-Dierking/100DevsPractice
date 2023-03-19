419cd4fbc7e806e9655605b3c255ed46a8adc875
Katrina <katrina@hertimetocode.com>
2023-03-18 23:05:33 -0400

    lesson 14 class work

--- src/class14-materials/bring-it-home-practice/js/main.js
+++ src/class14-materials/bring-it-home-practice/js/main.js
@@ -1,14 +1,59 @@
 // *Variables*
 // Create a variable and console log the value
 
+const name = 'katrina';
+
+console.log(name)
+
 // Create a variable, add 10 to it, and alert the value
+
+let num = 5; 
+
+num +=10
+
+alert (num)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
+function sub (a,b,c,d) {
+
+    return result = a-b-c-d
+}
+
+alert(sub(5,6,7,4))
+
 // Create a function that divides one number by another and returns the remainder
+
+function div(a,b) {
+    return a%b 
+
+}
+alert (div(10,4))
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
+function add(a,b) {
+    if(a+b > 50) {
+        console.log("Jumanji")
+    } else {
+        console.log("oz")
+    }
+}
+
+alert(add(5,8))
+alert(add(45,15))
+
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+
+function mult(a,b,c) {
+    if(a*b*c % 3 == 0) {
+        alert ('zebra')
+    } else {
+        alert ('no')
+    }
+}
+
+mult(5,6,7)
+mult(2,2,5)
--- src/class14-materials/temperature-converter/js/main.js
+++ src/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,20 @@
 //Write your pseduo code first! 
+
+/// 1. input the temp in cels
+/// 2. use formula to convert
+//// 3. Display
+
+document.querySelector("#convert").addEventListener("click", run);
+
+function run() {
+
+    // console.log("YAY IT WORKS")
+    let temp = document.querySelector('#cels').value 
+
+   temp = temp * 9/5 + 32
+
+    document.querySelector('#placeToConvert').innerText = temp
+}
+
+
+
