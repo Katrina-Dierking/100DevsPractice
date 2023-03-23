ba2d1565df09432b2efdadec3af75e86ed9a0e0c
Katrina <katrina@hertimetocode.com>
2023-03-23 17:01:35 -0400

    class 19 practice

--- src/class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ src/class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -13,8 +13,8 @@ console.log(color)
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
 
 function add(a,b,c,d) {
-    prod = (a+b+c)/d
-    return prod
+   return (a+b+c)/d
+   
 }
 
 console.log(add(1,2,3,4))
@@ -36,22 +36,24 @@ power(5,6)
 // OR
 const booString = (boo, str) => boo ? alert(str) : console.log(str)
 
+booString(5, "hi")
+
+
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
 
 function fizzbuzz(num) {
     for (let i = 1; i <= num; i++) {
-        console.log(i)
 
-        if(i % 3 === 0) {
-            console.log("Fizz")
-        } else if(i % 5 === 0) {
-            console.log("Buzz")
-        }else if (i % 3 === 0 && i % 5 === 0) {
-            console.log("fizzBuzz")
+        if (i % 3 === 0 && i % 5 === 0) {
+          console.log("FizzBuzz");
+        } else if (i % 5 === 0) {
+          console.log("Buzz");
+        } else if (i % 3 === 0 ) {
+          console.log("fizz");
         } else {
-            console.log(i)
+          console.log(i);
         }
     }
 }
