a6799d9f00906d42e1dc5af8a28ac8cca6865818
Katrina <katrina@hertimetocode.com>
2023-03-23 22:11:14 -0400

    class 19 cont

--- src/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ src/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,17 +1,28 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
+//Create an array of movie titles. Loop through the array and add each element to the h2.
 
-const movies = ["harry potter", "star wars", "cars"]
+const movies = ["harry potter", " star wars", " cars"]
 
-for (let i =0; i < movies.length; i++) {
+// movies.forEach((x, i) => console.log(x))
 
+for (let i = 0; i < movies.length; i++) {
+    document.querySelector('h2').innerText += movies[i]
 }
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
 
-const nums = [1, 2,3, 4, 5] 
+let nums = [1, 2,3, 4, 5] 
 
-for (let i = 0; i < nums.length; i++) {
-    
+nums.forEach((x, i) => {
+    nums[i] += 3
+})
+
+console.log(nums)
+
+//Find the average of all the numbers from question two
+
+let sum = 0; 
+
+for (let i = 0; i<nums.length; i++) {
+    sum += nums[i] 
 }
 
-
-//Find the average of all the numbers from question three
+console.log(sum/nums.length)
