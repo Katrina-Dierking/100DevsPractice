89b09e691ba6802eedbfa05c49765a07b49eb9c8
Katrina <katrina@hertimetocode.com>
2023-03-26 20:33:25 -0400

    started media queries for salon layout-

---
+++ src/layout-practice/hair-salon/js/main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function main() {
+  return (
+    <header>
+        <p>Ayesha.</p>
+			<nav>
+				<a href = "#">HOME</a>
+				<a href = "#">ABOUT</a>
+				<a href = "#">SERVICES</a>
+				<a href = "#">LOOKBOOK</a>
+				<a href = "#">NEWS</a>
+				<a href = "#">CONTACT</a>
+			</nav>
+			<p>- BOOK ONLINE</p>
+    </header>
+  )
+}
+
+export default main
