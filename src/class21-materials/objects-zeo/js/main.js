368f0fecf036fb74b25a0ff637aab45f7ab3df76
Katrina <katrina@hertimetocode.com>
2023-03-29 23:06:53 -0400

    object practice

--- src/class21-materials/objects-zeo/js/main.js
+++ src/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,21 @@
 //Create a stopwatch object that has four properties and three methods
+
+const stopwatch = {}
+
+stopwatch.color = "black"
+stopwatch.shape = "round"
+stopwatch.brand = "Nikola"
+stopwatch.size = "large"
+
+stopwatch.start = function() {
+    console.log('ready, set, go')
+}
+
+stopwatch.stop = function() {
+    console.log('time is up')
+}
+
+stopwatch.sayBrand = function () {
+    console.log(stopwatch.brand)
+}
+
