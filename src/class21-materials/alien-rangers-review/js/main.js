4e12ed418e6a9dbea7398a77220f2a99d8e2b54c
Katrina <katrina@hertimetocode.com>
2023-03-29 22:27:45 -0400

    continued practice 21

--- src/class21-materials/alien-rangers-review/js/main.js
+++ src/class21-materials/alien-rangers-review/js/main.js
@@ -2,8 +2,16 @@
 
 //Create and array of tv shows. Loop through and print each show to the console
 
+const tv = ['That 70s', 'Hawaii five-0', 'friends']
+
+tv.forEach(show => console.log( show ))
+
+
+
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+
+// map
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
