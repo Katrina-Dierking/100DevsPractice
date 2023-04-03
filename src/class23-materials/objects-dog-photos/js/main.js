0497fcac7d7ea20741798cffd73e1352c04b5698
Katrina <katrina@hertimetocode.com>
2023-04-03 12:42:13 -0400

    dog api cont

--- src/class23-materials/objects-dog-photos/js/main.js
+++ src/class23-materials/objects-dog-photos/js/main.js
@@ -3,7 +3,8 @@
 fetch("https://dog.ceo/api/breeds/image/random") 
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data.message) //message is a property on the dog api object
+        document.querySelector('img').src = data.message // gets different img each time. Each image has a new img url
     })
     .catch(err => {
         console.log(`err ${err}`)
