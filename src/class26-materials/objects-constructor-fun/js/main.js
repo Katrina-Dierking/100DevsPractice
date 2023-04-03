4094e6685fc3e1705df134dcd2e55dd26da3afec
Katrina <katrina@hertimetocode.com>
2023-04-03 13:55:37 -0400

    cont constructor practice

--- src/class26-materials/objects-constructor-fun/js/main.js
+++ src/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+
+function Crafter (name, age, price, tools) {
+    this.name = name
+    this.age = age 
+    this.price = price
+    this.tools = tools
+
+    this.speak = function(){
+        console.log (`${name} says its time to create`)
+    }
+    this.sells = function(){
+    console.log(`${name} sells her items for ${price}`)
+    }
+}
+
+
+let Joann = new Crafter('joann', 34, '$18', ['clay', 'paint', 'glue'] )
+
+console.log(Joann.sells())
