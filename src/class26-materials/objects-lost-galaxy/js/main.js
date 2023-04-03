ebeaabac18ce6df9cfdc35e6d14ec2a39c982c69
Katrina <katrina@hertimetocode.com>
2023-04-03 13:10:55 -0400

    obj practice

--- src/class26-materials/objects-lost-galaxy/js/main.js
+++ src/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods (without constructor)
+
+let pizza = {}
+
+pizza.size = "medium"
+pizza.topping = "pepporoni"
+pizza.price = "$12.00"
+pizza.store = "dominos"
+pizza.feed = function() {
+    console.log("feeds a family of four")
+}
+pizza.pairing = function() {
+    console.log("goes good with beer")
+}
+pizza.effect = function(){
+    console.log(`${pizza.topping} pizza gives me heart burn`)
+}
+
+console.log(pizza.effect())
