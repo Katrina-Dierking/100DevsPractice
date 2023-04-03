5484dccd257aa5bd37b6923eb705b4ed74e9c276
Katrina <katrina@hertimetocode.com>
2023-04-03 12:17:20 -0400

    constructor practice

--- src/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ src/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,2 +1,23 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
 
+function TonyHawkProSkater (age, gender, level, awards) {
+    this.age = age; 
+    this.gender = gender; 
+    this.level = level; 
+    this.awars = awards; 
+    this.jump = function() {
+        console.log("look how high he can go")
+    }
+    this.pain = function() {
+        console.log('woooo, he got hurt')
+    }
+    this.speak = function(){
+        console.log('thank you for coming to my show')
+    }
+}
+
+let Jon = new TonyHawkProSkater(21, "male", "amature", "no awards yet")
+
+console.log(Jon)
+console.log(Jon.speak())
+console.log(Jon.jump())
