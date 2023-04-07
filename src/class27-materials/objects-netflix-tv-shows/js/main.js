f22459ee3f79afb3a33db74af03bbe9b3414be35
Katrina <katrina@hertimetocode.com>
2023-04-06 20:59:31 -0400

    class 27

--- src/class27-materials/objects-netflix-tv-shows/js/main.js
+++ src/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,33 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
 
+class Netflix {
+    
+    constructor(title, time, day, genre) {
+    this.title = title
+    this.time = time
+    this.day = day
+    this.genre = genre 
+
+}
+    start() {
+        console.log(`${this.title} starts at ${this.time}`)
+    }
+
+    stop() {
+        console.log(`${this.title} has ended.`)
+    }
+
+    awards() {
+        console.log(`${this.title} gets awards in ${this.genre}`)
+    }
+
+    tuneIn(){
+        console.log(`${this.title} can be viewed on ${this.day} at ${this.time}.`)
+    }
+}
+
+let showOne = new Netflix('Virgin River', ' 8pm', 'Friday', 'RomCom')
+
+console.log(showOne.tuneIn())
+console.log(showOne.stop())
