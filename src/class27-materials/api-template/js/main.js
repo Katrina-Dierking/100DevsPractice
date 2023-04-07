f91fdc2d5358b1e445bab18376fe193b021b0cb2
Katrina <katrina@hertimetocode.com>
2023-04-07 12:13:58 -0400

    27 nasa practice

--- src/class27-materials/api-template/js/main.js
+++ src/class27-materials/api-template/js/main.js
@@ -3,13 +3,18 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+
+  // const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+
+  const url =
+    `https://api.nasa.gov/planetary/apod?api_key=vGGeAuuDS0BbdMmB5VLudDDxigAHEPOYoAtMlbmU&date=${choice}`;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.results[0].name)
-        console.log('its working')
+        console.log(data)
+      document.querySelector('img').src = data.hdurl
+      document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
