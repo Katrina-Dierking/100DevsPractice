da90f23d288ce26220bd0f196bc70765c28f6b2a
Katrina <katrina@hertimetocode.com>
2023-04-17 16:26:41 -0400

    card-game api debugging

--- src/class28-materials/card-game/js/main.js
+++ src/class28-materials/card-game/js/main.js
@@ -1,6 +1,7 @@
 //Get the deck
 let deckId = ''
 
+
 fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) // parse response as JSON
       .then(data => {
@@ -13,7 +14,8 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 
 
 document.querySelector('button').addEventListener('click', getFetch)
-
+    document.querySelector('#player1').src = localStorage.getItem("card1")
+    document.querySelector('#player2').src = localStorage.getItem("card2")
 function getFetch(){
   
   const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
@@ -24,16 +26,29 @@ function getFetch(){
         console.log(data)
         let val1 = Number(cardValue( data.cards[0].value ))
         let val2 = Number(cardValue( data.cards[1].value ))
-        document.querySelector('#player1').src = data.cards[0].image
-        document.querySelector('#player2').src = data.cards[1].image
-        if(val1 > val2){
-          document.querySelector('h3').innerText = 'Player 1 WON!'
-        }else if(val1 < val2){
-          document.querySelector('h3').innerText = 'Player 2 WON!'
-        }else{
-          document.querySelector('h3').innerText = 'WAR!'
+        // document.querySelector('#player1').src = data.cards[0].image
+        // document.querySelector('#player2').src = data.cards[1].image
+
+        if(!localStorage.getItem('card1') && !localStorage.getItem('card2')) {
+            localStorage.setItem('card1', Number(cardValue(data.cards[0].value )));
+            localStorage.setItem('card2', Number(cardValue(data.cards[1].value)));
+          if(val1 > val2){
+            document.querySelector('h3').innerText = 'Player 1 WON!'
+          }else if(val1 < val2){
+            document.querySelector('h3').innerText = 'Player 2 WON!'
+          }else{
+            document.querySelector('h3').innerText = 'WAR!'
+          }
+        } else {
+          let card1 = localStorage.getItem('card1') + Number(cardValue(data.cards[0].value))
+          let card2 = localStorage.getItem('card2') + Number(cardValue(data.cards[1].value))
+          localStorage.setItem('card1', card1)
+          localStorage.setItem('card2', card2)
+        
         }
         
+            document.querySelector('h3').innerText = localStorage.getItem('card1')
+            document.querySelector('h3').innerText = localStorage.getItem('card2')
       })
       .catch(err => {
           console.log(`error ${err}`)
